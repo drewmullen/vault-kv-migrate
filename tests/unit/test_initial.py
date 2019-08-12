c6730468e332872be34e287e2ace0312320b1ba8
Robert James Hernandez <rob@sarcasticadmin.com>
2019-08-12 12:29:54 -0700

    Adding tox and moving initial tests

--- tests/unit/test_initial.py
+++ tests/unit/test_initial.py
@@ -15,28 +15,28 @@ def test_trailing_slash():
 
 ## recursive_path_builder
 
-@mock.patch('kv_recursive.list_path')
-def test_recursive_path_buider(mock_list_path):
-    client = ""
-    kv_list = ['test/', 'test1']
-    kv_version = 1
-    source_mount = 'secret'
-    
-    mock_list_path.return_value = ''
-
-    kv_recursive.recursive_path_builder(client, kv_list, kv_version, source_mount)
-
-    assert kv_recursive.list_path.called
-    # assert kv_recursive.recursive_path_builder.called
-
-
-@mock.patch('hvac.api.secrets_engines.kv_v1.list_secrets')
-def test_list_path(mock_kv)
-
-kv_version = 1
-list_path()
+#@mock.patch('kv_recursive.list_path')
+#def test_recursive_path_buider(mock_list_path):
+#    client = ""
+#    kv_list = ['test/', 'test1']
+#    kv_version = 1
+#    source_mount = 'secret'
+#    
+#    mock_list_path.return_value = ''
+#
+#    kv_recursive.recursive_path_builder(client, kv_list, kv_version, source_mount)
+#
+#    assert kv_recursive.list_path.called
+#    # assert kv_recursive.recursive_path_builder.called
 
 
+#@mock.patch('hvac.api.secrets_engines.kv_v1.list_secrets')
+#def test_list_path(mock_kv)
+#
+#kv_version = 1
+#list_path()
+#
+#
 # kv_list has test/, ensure list_path is called then ensure recursive_path_builder is called
 
 # kv_list has no ending /s, ensure neither list_path or recursive_ are called
