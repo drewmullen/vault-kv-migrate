6f404d291b015c448d003cc7c272e3d3df3f5fcb
Drew Mullen <drew@nebulaworks.com>
2019-08-12 16:19:40 -0400

    list_path tests

--- tests/unit/test_initial.py
+++ tests/unit/test_initial.py
@@ -1,50 +1,46 @@
 import kv_recursive
-import pytest
 import mock
-import hvac
-
-### Tests
-
-## ensure trailing slash
-
-def test_trailing_slash():
-    assert kv_recursive.ensure_trailing_slash("test/") == "test/"
-    assert kv_recursive.ensure_trailing_slash("test") == "test/"
-    assert kv_recursive.ensure_trailing_slash("test/super") == "test/super/"
-    assert kv_recursive.ensure_trailing_slash("test/super/") == "test/super/"
-
-## recursive_path_builder
-
-#@mock.patch('kv_recursive.list_path')
-#def test_recursive_path_buider(mock_list_path):
-#    client = ""
-#    kv_list = ['test/', 'test1']
-#    kv_version = 1
-#    source_mount = 'secret'
-#    
-#    mock_list_path.return_value = ''
-#
-#    kv_recursive.recursive_path_builder(client, kv_list, kv_version, source_mount)
-#
-#    assert kv_recursive.list_path.called
-#    # assert kv_recursive.recursive_path_builder.called
 
 
-#@mock.patch('hvac.api.secrets_engines.kv_v1.list_secrets')
-#def test_list_path(mock_kv)
-#
-#kv_version = 1
-#list_path()
-#
-#
-# kv_list has test/, ensure list_path is called then ensure recursive_path_builder is called
+class TestKV(object):
 
-# kv_list has no ending /s, ensure neither list_path or recursive_ are called
+    def setup_method(self, method):
+        # anytime client is called we can pass hvacclient
+        self.hvacclient = mock.patch('hvac.Client').start()
 
-## list_path
+    def teardown_method(self, method):
+        self.hvacclient.stop()
 
-# pass a list with secret/test, mock return to read_secret() and enssure properformat of output
+    def test_trailing_slash(self):
+        assert kv_recursive.ensure_trailing_slash("test/") == "test/"
+        assert kv_recursive.ensure_trailing_slash("test") == "test/"
+        assert kv_recursive.ensure_trailing_slash("test/super") == "test/super/"
+        assert kv_recursive.ensure_trailing_slash("test/super/") == "test/super/"
 
-## read_secrets_from_list
+    # list_path
 
-# pass a list with secret/test, mock return to read_secret() and enssure properformat of output
+    def test_list_path_v2(self):
+        self.hvacclient.secrets.kv.v2.list_secrets.return_value = {'data': {'keys': ['test2']}}
+        kv_list = kv_recursive.list_path(
+                self.hvacclient,
+                path='',
+                kv_version=2,
+                source_mount='secret',
+                kv_list=['test']
+            )
+
+        assert kv_list == ['test', 'test2']
+        assert self.hvacclient.secrets.kv.v2.list_secrets.called
+
+    def test_list_path_v1(self):
+        self.hvacclient.secrets.kv.v1.list_secrets.return_value = {'data': {'keys': ['test2']}}
+        kv_list = kv_recursive.list_path(
+                self.hvacclient,
+                path='',
+                kv_version=1,
+                source_mount='secret',
+                kv_list=['test']
+            )
+
+        assert kv_list == ['test', 'test2']
+        assert self.hvacclient.secrets.kv.v1.list_secrets.called
