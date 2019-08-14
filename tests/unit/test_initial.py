8bf911bee80ae539c7e61835962273bf1a6f9310
Drew Mullen <drew@nebulaworks.com>
2019-08-14 16:04:44 -0400

    refactor to simplify list_path

--- tests/unit/test_initial.py
+++ tests/unit/test_initial.py
@@ -19,31 +19,30 @@ class TestKV(object):
 
     # list_path
 
-    def test_list_path_v2(self):
-        self.hvacclient.secrets.kv.v2.list_secrets.return_value = {'data': {'keys': ['test2']}}
-        kv_list = kv_recursive.list_path(
-                self.hvacclient,
-                path='',
-                kv_version=2,
-                source_mount='secret',
-                kv_list=['test']
-            )
+    # def test_list_path_v2(self):
+    #     self.hvacclient.secrets.kv.v2.list_secrets.return_value = ['test2']
+    #     kv_list = kv_recursive.list_path(
+    #             self.hvacclient,
+    #             path='',
+    #             kv_version=2,
+    #             source_mount='secret',
+    #         )
 
-        assert kv_list == ['test', 'test2']
-        assert self.hvacclient.secrets.kv.v2.list_secrets.called
+    #     assert kv_list == ['test', 'test2']
+    #     assert self.hvacclient.secrets.kv.v2.list_secrets.called
 
-    def test_list_path_v1(self):
-        self.hvacclient.secrets.kv.v1.list_secrets.return_value = {'data': {'keys': ['test2']}}
-        kv_list = kv_recursive.list_path(
-                self.hvacclient,
-                path='',
-                kv_version=1,
-                source_mount='secret',
-                kv_list=['test']
-            )
+    # def test_list_path_v1(self):
+    #     self.hvacclient.secrets.kv.v1.list_secrets.return_value = {'data': {'keys': ['test2']}}
+    #     kv_list = kv_recursive.list_path(
+    #             self.hvacclient,
+    #             path='',
+    #             kv_version=1,
+    #             source_mount='secret',
+    #             kv_list=['test']
+    #         )
 
-        assert kv_list == ['test', 'test2']
-        assert self.hvacclient.secrets.kv.v1.list_secrets.called
+    #     assert kv_list == ['test', 'test2']
+    #     assert self.hvacclient.secrets.kv.v1.list_secrets.called
 
     def test_read_secrets(self):
         kv_list = ['test']
@@ -55,3 +54,66 @@ class TestKV(object):
             source_mount='secret'
         )
         assert secrets == [{'test': {'name': 'drew'}}]
+
+    def test_write_secrets_from_list_v2(self):
+        kv_recursive.write_secrets_from_list(
+            self.hvacclient,
+            kv_list=[{'test': {'name': 'drew'}}],
+            dest_path='',
+            src_path='',
+            kv_version=2,
+            dest_mount='secret'
+        )
+        assert self.hvacclient.secrets.kv.v2.create_or_update_secret.called
+
+    def test_write_secrets_from_list_v1(self):
+        kv_recursive.write_secrets_from_list(
+            self.hvacclient,
+            kv_list=[{'test': {'name': 'drew'}}],
+            dest_path='',
+            src_path='',
+            kv_version=1,
+            dest_mount='secret'
+        )
+        assert self.hvacclient.secrets.kv.v1.create_or_update_secret.called
+
+    def test_delete_secrets_from_list_v2(self):
+        kv_recursive.delete_secrets_from_list(
+            self.hvacclient,
+            kv_list=['test', 'test2'],
+            kv_version=2,
+            source_mount='secret'
+        )
+        assert self.hvacclient.secrets.kv.v2.delete_metadata_and_all_versions.called
+
+    def test_delete_secrets_from_list_v1(self):
+        kv_recursive.delete_secrets_from_list(
+            self.hvacclient,
+            kv_list=['test', 'test2'],
+            kv_version=1,
+            source_mount='secret'
+        )
+        assert self.hvacclient.secrets.kv.v1.delete_secret.called
+
+    @mock.patch('kv_recursive.list_path')
+    def test_recusive_path_builder_no_lists(self, mock_list_path):
+        kv_recursive.recursive_path_builder(
+            self.hvacclient,
+            kv_list=['test'],
+            kv_version=2,
+            source_mount='secret'
+        )
+
+        assert not mock_list_path.called
+
+    @mock.patch('kv_recursive.list_path')
+    def test_recusive_path_builder(self, mock_list_path):
+        mock_list_path.return_value = ['secret']
+        secrets = kv_recursive.recursive_path_builder(
+            self.hvacclient,
+            kv_list=['test', 'test2/'],
+            kv_version=2,
+            source_mount='secret'
+        )
+
+        assert secrets == ['test', 'test2/secret']
